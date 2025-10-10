159381b8d3a8cc9dbe465ec042a0878a3110676b
Jefferson <adrianolape@protonmail.com>
2025-10-11 02:44:31 +0800

    ✨ Add staff picks section to home endpoint response

--- controllers/home.rb
+++ controllers/home.rb
@@ -16,6 +16,7 @@ module Home
       document = Nokogiri::HTML(response.body)
       spotlight = []
       popular = []
+      staff_picks = []
       latest_updates = []
 
       document.css('.mantine-Carousel-slide').each do |elem|
@@ -35,12 +36,7 @@ module Home
         genres = elem.css('.mantine-Badge-root a').map(&:text).map(&:strip).reject(&:empty?)
         next if title.empty? || id.nil? || img_url.nil? || genres.empty?
 
-        spotlight << { 
-          id:, 
-          title:, 
-          img_url:, 
-          genres:
-        }
+        spotlight << { id: id, title: title, img_url: img_url, genres: genres }
       end
 
       if (popular_section = document.at_css('#popular'))
@@ -83,6 +79,46 @@ module Home
         end
       end
 
+      if (staff_section = document.at_css('#staff-picks'))
+        staff_section.css('.mantine-Grid-col').each do |elem|
+          link = elem.at_css('a[href^="/series/"]')
+          next unless link
+
+          id = link['href'].split('/').last
+          title = elem.at_css('p.mantine-Text-root')&.text&.strip || ''
+          img_elem = elem.at_css('.SeriesCard_imageContainer__Tjx97 img')
+          img_src = img_elem ? img_elem['src'] : nil
+          img_url = nil
+
+          if img_src
+            uri = URI.parse(img_src)
+            query = URI.decode_www_form(uri.query || '').to_h
+            img_url = query['url'] ? CGI.unescape(query['url']) :
+                      (img_src.start_with?('/') ? URI.join('https://cdn.flamecomics.xyz', img_src).to_s : img_src)
+          end
+
+          status = elem.at_css('.mantine-Badge-root[data-variant="outline"] .mantine-Badge-label')&.text&.strip
+          likes_text = elem.at_css('svg.bi-heart-fill')&.parent&.text&.strip
+          likes = if likes_text
+            if likes_text.include?('K')
+              (likes_text.delete('K').to_f * 1000).to_i
+            else
+              likes_text.gsub(/[^\d]/, '').to_i
+            end
+          end
+
+          next if title.empty? || id.nil? || img_url.nil?
+
+          staff_picks << {
+            id:,
+            title:,
+            img_url:,
+            status:,
+            likes:
+          }
+        end
+      end
+
       document.css('.m_96bdd299.mantine-Grid-col').each do |elem|
         series_title_a = elem.at_css('a.mantine-Text-root[data-size="md"]')
         title = series_title_a&.text&.strip
@@ -107,7 +143,7 @@ module Home
           chapter_id = chapter_link['href'].split('/').last
           chapter_date = chapter_link.at_css('p.SeriesCard_date__wbLsz')&.text&.strip
           next if chapter_title.nil? || chapter_title.empty? || chapter_id.nil? || chapter_id.empty? || chapter_date.nil? || chapter_date.empty?
-          { chapter_id:, chapter_title:, chapter_date: }
+          { chapter_id: chapter_id, chapter_title: chapter_title, chapter_date: chapter_date }
         end.compact
 
         next if chapters.empty? || img_url.nil?
@@ -117,13 +153,14 @@ module Home
           title:, 
           img_url:, 
           status:, 
-          chapters: 
-        }
+          chapters:
+       }
       end
 
       {
         'spotlight' => spotlight,
         'popular' => popular,
+        'staff_picks' => staff_picks,
         'latest_updates' => latest_updates
       }
 
