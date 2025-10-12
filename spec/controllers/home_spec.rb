6e5fe6b4b8936ffdd4af950285e1679637105157
Jefferson <adrianolape@protonmail.com>
2025-10-12 10:37:28 +0800

    ✨ Update home_spec to improve error handling and streamline HTML response parsing

--- spec/controllers/home_spec.rb
+++ spec/controllers/home_spec.rb
@@ -5,24 +5,40 @@ require_relative '../../controllers/home'
 
 RSpec.describe Home do
   describe '.fetch_data' do
+    let(:html_response) do
+      <<-HTML
+        <html>
+          <body>
+            <div class="mantine-Title-root" data-order="2">Spotlight</div>
+            <div class="mantine-Title-root" data-order="2">Popular</div>
+            <div class="mantine-Title-root" data-order="2">Staff Picks</div>
+            <div class="mantine-Title-root" data-order="2">Latest Updates</div>
+          </body>
+        </html>
+      HTML
+    end
+
     context 'when HTTP request fails' do
       it 'returns an error hash' do
-        allow(HTTParty).to receive(:get).and_raise(StandardError.new("Network error"))
-
+        allow(HTTParty).to receive(:get).and_return(double(code: 500))
         result = Home.fetch_data
-        expect(result[:error] || result["error"]).to match(/Network error/)
+        expect(result).to be_a(Hash)
+        expect(result).to have_key(:error)
       end
     end
 
     context 'when HTTP request succeeds' do
       it 'returns parsed HTML data' do
-        fake_html = '<html><body><div id="Popular"></div></body></html>'
-        fake_response = double('response', code: 200, body: fake_html)
-        allow(HTTParty).to receive(:get).and_return(fake_response)
-
+        allow(HTTParty).to receive(:get).and_return(double(code: 200, body: html_response))
         result = Home.fetch_data
+
         expect(result).to be_a(Hash)
-        expect(result.keys).to include('spotlight', 'popular', 'latest_updates')
+        expect(result.keys).to include(:spotlight, :popular, :staff_picks, :latest_updates)
+
+        expect(result[:spotlight]).to be_a(Array)
+        expect(result[:popular]).to be_a(Array)
+        expect(result[:staff_picks]).to be_a(Array)
+        expect(result[:latest_updates]).to be_a(Array)
       end
     end
   end
