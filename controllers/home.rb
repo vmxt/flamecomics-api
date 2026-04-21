ca464183b4dbb830c17ad7a53d48b7ed7c5a76c4
Jefferson <adrianolape@protonmail.com>
2026-04-22 02:40:15 +0800

    Update home.rb

--- controllers/home.rb
+++ controllers/home.rb
@@ -4,10 +4,8 @@ require 'httparty'
 require 'nokogiri'
 require 'cgi'
 require 'uri'
-require 'time'
 require_relative '../utils/variables'
 require_relative '../utils/image_helper'
-require_relative '../utils/time_helper'
 
 module Home
   extend self
@@ -24,7 +22,8 @@ module Home
       spotlight: extract_spotlight(doc),
       popular: extract_cards(doc, '#popular'),
       staff_picks: extract_cards(doc, '#staff-picks'),
-      latest_updates: extract_latest_updates(doc)
+      latest_updates: extract_latest_updates(doc),
+      novels: extract_novels(doc)
     }
   rescue StandardError => e
     { error: "Error fetching data: #{e.message}" }
@@ -33,87 +32,76 @@ module Home
   private
 
   def extract_spotlight(doc)
-    slides = doc.css('div[data-orientation="horizontal"]').filter_map do |slide|
+    doc.css('div[data-orientation="horizontal"]').filter_map do |slide|
       link = slide.at_css('a[href^="/series/"]')
-      href = link&.[]('href')
-      id = href&.split('/')&.last
-      next unless link && id
+      id = link&.[]('href')&.split('/')&.last
+      next unless id
 
       title = slide.at_css('h2, h3')&.text&.strip
-      next if title.nil? || title.empty?
+      next if title.to_s.empty?
 
       img_url = normalize_image_url(slide.at_css('img')&.[]('src'))
       next unless img_url
 
-      genres = slide.css('a[href^="/genre/"] > div > span')
+      genres = slide.css('a[href^="/genre/"] span')
                     .map { |x| x.text.strip }
                     .reject(&:empty?)
                     .uniq
 
       { id: id, title: title, img_url: img_url, genres: genres }
     end
-
-    slides.uniq { |s| s[:id] }
   end
 
   def extract_cards(doc, selector)
     section = doc.at_css(selector)
     return [] unless section
 
-    section.css('.mantine-Grid-col').filter_map do |elem|
+    section.css('.m_96bdd299.mantine-Grid-col').filter_map do |elem|
       link = elem.at_css('a[href^="/series/"]')
       next unless link
 
-      href = link['href']
-      id = href&.split('/')&.last
-      title = elem.at_css('p.mantine-Text-root')&.text&.strip
+      id = link['href']&.split('/')&.last
+      title = elem.at_css('h3 span')&.text&.strip
+
       img_url = normalize_image_url(elem.at_css('img')&.[]('src'))
       next unless id && title && img_url
 
-      status = elem.at_css('.mantine-Badge-label')&.text&.strip
-      likes_el = elem.at_css('svg.bi-heart-fill')
-      likes_text = likes_el ? likes_el.parent&.text&.strip : nil
-      likes = parse_likes(likes_text)
+      language = elem.at_css('[data-nosnippet] span')&.text&.strip
+      language = nil if language&.empty?
 
-      { id: id, title: title, img_url: img_url, status: status, likes: likes }
+      likes = parse_likes(elem.at_css('svg + span')&.text)
+
+      {
+        id: id,
+        title: title,
+        img_url: img_url,
+        language: language,
+        likes: likes
+      }
     end
   end
 
   def extract_latest_updates(doc)
     doc.css('.m_96bdd299.mantine-Grid-col').filter_map do |elem|
       title_a = elem.at_css('a.mantine-Text-root[data-size="md"]')
-      href = title_a&.[]('href')
-      id = href&.split('/')&.last
+      id = title_a&.[]('href')&.split('/')&.last
       title = title_a&.text&.strip
       next unless id && title
 
       img_url = normalize_image_url(elem.at_css('img')&.[]('src'))
       next unless img_url
 
-      chapters = elem.css("a[href*=\"/series/#{id}/\"]").filter_map do |link|
-        ch_title = link.at_css('p')&.text&.strip
-        ch_href = link['href']
-        ch_id = ch_href&.split('/')&.last
+      language = elem.at_css('[data-nosnippet] span')&.text&.strip
+      language = nil if language&.empty?
 
-        raw_date = link.at_css('p.SeriesCard_date__wbLsz')&.[]('title') ||
-                   link.at_css('p[data-size="xs"]')&.[]('title') ||
-                   link.at_css('p.SeriesCard_date__wbLsz')&.text ||
-                   link.at_css('p[data-size="xs"]')&.text
-
-        ch_date = if raw_date
-                    begin
-                      time_obj = Time.parse(raw_date)
-                      TimeHelper.time_ago_in_words(time_obj)
-                    rescue ArgumentError
-                      raw_date.strip
-                    end
-                  else
-                    'Unknown'
-                  end
-
-        next unless ch_title && ch_id && ch_date
-
-        { chapter_id: ch_id, chapter_title: ch_title, chapter_date: ch_date }
+      chapters = elem.css('a[href^="/series/"]').select do |a|
+        a['href'].split('/').length >= 4
+      end.map do |link|
+        {
+          chapter_id: link['href'].split('/').last,
+          chapter_title: link.at_css('p')&.text&.strip,
+          chapter_date: nil
+        }
       end
 
       next if chapters.empty?
@@ -122,8 +110,39 @@ module Home
         id: id,
         title: title,
         img_url: img_url,
-        status: elem.at_css('.mantine-Badge-label')&.text&.strip,
+        language: language,
         chapters: chapters
+      }
+    end
+  end
+
+  def extract_novels(doc)
+    section = doc.at_css('#novels')
+    return [] unless section
+
+    section.css('.m_96bdd299.mantine-Grid-col').filter_map do |elem|
+      link = elem.at_css('a[href^="/novel/"]')
+      next unless link
+
+      id = link['href']&.split('/')&.last
+      title = elem.at_css('h3 span')&.text&.strip
+
+      img_url = normalize_image_url(elem.at_css('img')&.[]('src'))
+      next unless id && title && img_url
+
+      language = elem.at_css('[data-nosnippet] span')&.text&.strip
+      language = nil if language&.empty?
+
+      likes = parse_likes(elem.at_css('svg + span')&.text)
+      status = elem.css('.mantine-Badge-label').last&.text&.strip
+
+      {
+        id: id,
+        title: title,
+        img_url: img_url,
+        language: language,
+        likes: likes,
+        status: status
       }
     end
   end
@@ -132,5 +151,19 @@ module Home
     return unless text
 
     text.include?('K') ? (text.delete('K').to_f * 1000).to_i : text.gsub(/[^\d]/, '').to_i
+  end
+
+  def normalize_image_url(url)
+    return unless url
+
+    if url.include?('/_next/image')
+      decoded = CGI.unescape(url)
+      match = decoded.match(%r{url=(https?://[^&]+)})
+      return match[1] if match
+    end
+
+    url.start_with?('//') ? "https:#{url}" : url
+  rescue StandardError
+    url
   end
 end
